415842eebd9349ee062e8807b1e5cf5e31e7b473
Thomas CHALMEL (Shmii) <thomas@chalmel.org>
2025-07-24 15:09:26 +0200

    feat(molecule): Add support of bootstrap script for Vagrant/libvirt

---
+++ molecule/default/cento-7.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo sed -i \
+-e 's|^mirrorlist=|#mirrorlist=|' \
+-e 's|^#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|' \
+/etc/yum.repos.d/CentOS-Base.repo
+
+sudo yum clean all
+sudo yum makecache
+sudo yum repolist
+sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel wget make
+
+cd /usr/src
+
+sudo wget https://www.python.org/ftp/python/3.8.18/Python-3.8.18.tgz
+sudo tar xzf Python-3.8.18.tgz
+
+cd Python-3.8.18
+
+sudo ./configure --enable-optimizations
+sudo make altinstall
+sudo ln -sf /usr/local/bin/python3.8 /usr/bin/python3
+sudo ln -sf /usr/local/bin/pip3.8 /usr/bin/pip3
